39a87dff07c05782469a20ea74c69f594c4ad121
Jochen Mehlich <contact@jochenmehlich.de>
2023-11-03 22:40:54 +0100

    added walking animation for player

--- player/player.gd
+++ player/player.gd
@@ -16,44 +16,67 @@ var down_pressed = 0
 var dir : Vector2
 
 func _unhandled_input(event):
+	var animationPlayer = $Visual/AnimationPlayer
 	if is_player_1:
 		if event is InputEventKey:
 			if event.is_action_pressed("walk_right_p1"):
 				right_pressed = 1
+				get_node("Visual").set_flip_h(false)
+				animationPlayer.play("player_walk")
 			if event.is_action_pressed("walk_left_p1"):
 				left_pressed = 1
+				get_node("Visual").set_flip_h(true)
+				animationPlayer.play("player_walk")
 			if event.is_action_pressed("walk_down_p1"):
 				down_pressed = 1
+				animationPlayer.play("player_walk")
 			if event.is_action_pressed("walk_up_p1"):
 				up_pressed = 1
-			
+				animationPlayer.play("player_walk")	
+				
+						
 			if event.is_action_released("walk_right_p1"):
 				right_pressed = 0
+				animationPlayer.stop()
 			if event.is_action_released("walk_left_p1"):
 				left_pressed = 0
+				animationPlayer.stop()
 			if event.is_action_released("walk_down_p1"):
 				down_pressed = 0
+				animationPlayer.stop()
 			if event.is_action_released("walk_up_p1"):
 				up_pressed = 0
+				animationPlayer.stop()
 	else:
 		if event is InputEventKey:
 			if event.is_action_pressed("walk_right_p2"):
 				right_pressed = 1
+				get_node("Visual").set_flip_h(false)
+				animationPlayer.play("player_walk")
 			if event.is_action_pressed("walk_left_p2"):
 				left_pressed = 1
+				get_node("Visual").set_flip_h(true)
+				print("test")
+				animationPlayer.play("player_walk")
 			if event.is_action_pressed("walk_down_p2"):
 				down_pressed = 1
+				animationPlayer.play("player_walk")
 			if event.is_action_pressed("walk_up_p2"):
 				up_pressed = 1
-			
+				animationPlayer.play("player_walk")			
+
 			if event.is_action_released("walk_right_p2"):
 				right_pressed = 0
+				animationPlayer.stop()
 			if event.is_action_released("walk_left_p2"):
 				left_pressed = 0
+				animationPlayer.stop()
 			if event.is_action_released("walk_down_p2"):
 				down_pressed = 0
+				animationPlayer.stop()
 			if event.is_action_released("walk_up_p2"):
 				up_pressed = 0
+				animationPlayer.stop()
 
 func _physics_process(delta):
 	dir = Vector2(
