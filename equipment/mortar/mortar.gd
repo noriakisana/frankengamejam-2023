cf5bb307a20b0bd2ec4c5229e37ab84f557991a8
Johannes <jojo@urweltpark.de>
2023-11-04 17:43:37 +0100

    mortar

--- equipment/mortar/mortar.gd
+++ equipment/mortar/mortar.gd
@@ -1,18 +1,46 @@
-extends Sprite2D
+extends Equipment
 
+@export var processing_time = 3
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	super()
+	$ProgressBar.visible = false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	super(delta)
+	if is_processing:
+		var bar_width = ((processing_time - $ProcessTimer.time_left) / processing_time) * 40.0
+		$ProgressBar.size.x = bar_width
 	
+func is_compatible(useable : Useable):
+	return useable is Ingredient and useable.type == Ingredient.Type.UNICORN
+
+func start_processing():
+	is_processing = true
+	$ProgressBar.visible = true
+	$ProcessTimer.wait_time = processing_time
+	$ProcessTimer.start()
+	$AnimationPlayer.play("mortar_animation")
+
+func _on_process_timer_timeout():
+	$ProgressBar.visible = false
+	if placed_useable.type == Ingredient.Type.UNICORN:
+		placed_useable = load("res://ingredients/unicorn.tres")
+		useable_node.queue_free()
+		useable_node = placed_useable.get_scene()
+		useable_node.scale = Vector2(0.7, 0.7)
+		add_child(useable_node)
+		is_processing = false
+		$AnimationPlayer.stop()
+		self.set_frame(0)
+
+"""
 func startAnimation():
 	$AnimationPlayer.start("mortar_animation")
 
 func stopAnimation():
 	$AnimationPlayer.stop()
 	self.set_frame(0)
+"""
