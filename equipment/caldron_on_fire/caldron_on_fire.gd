b961512af51930ba182be3249165c844c8e9d11d
Hannah <hannah.rattenberger@stud.uni-bamberg.de>
2023-11-04 21:06:33 +0100

    modified cauldron

--- equipment/caldron_on_fire/caldron_on_fire.gd
+++ equipment/caldron_on_fire/caldron_on_fire.gd
@@ -1,7 +1,8 @@
 extends Equipment
 
 @export var processing_time = 5
-
+var ingredients = []
+var recipe = [Ingredient.Type.CHOPPED_HEART, Ingredient.Type.FRIED_FROG]
 func _ready():
 	super()
 	$ProgressBar.visible = false
@@ -11,21 +12,33 @@ func _process(delta):
 	if is_processing:
 		var bar_width = ((processing_time - $ProcessTimer.time_left) / processing_time) * 40.0
 		$ProgressBar.size.x = bar_width
+		
 
 func is_compatible(useable : Useable):
 	return useable is Ingredient
 
-func start_processing():
+
+	
+	
+func process_potion():
 	is_processing = true
 	$ProgressBar.visible = true
 	$ProcessTimer.wait_time = processing_time
 	$ProcessTimer.start()
 	$AnimationPlayer.play("caldron_on_fire_animationw")
 
+func start_processing():
+	var fullfilled = true
+	for ingredient in recipe:
+		if !ingredients.has(ingredient):
+			fullfilled = false
+	if fullfilled:
+		process_potion()
+		
 func _on_process_timer_timeout():
 	$ProgressBar.visible = false
 	if placed_useable.type == Ingredient.Type.FROG:
-		placed_useable = load("res://ingredients/frog/frog.tres")
+		placed_useable = load("res://traenke/trank_green/green_potion.tres")
 		useable_node.queue_free()
 		useable_node = placed_useable.get_scene()
 		useable_node.scale = Vector2(0.7, 0.7)
