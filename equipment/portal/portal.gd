3741b5e38953c817144fce24dc9ce109de776806
Johannes <jojo@urweltpark.de>
2023-11-05 11:02:00 +0100

    score?

--- equipment/portal/portal.gd
+++ equipment/portal/portal.gd
@@ -13,6 +13,7 @@ func is_compatible(useable : Useable):
 
 func start_processing():
 	get_tree().get_nodes_in_group("label")[0].increaseScore(1)
+	#Score.increaseScore(1)
 	useable_node.queue_free()
 	placed_useable = null
 	#is_processing = true
--- score.gd
+++ score.gd
@@ -6,12 +6,12 @@ func resetScore():
 	score = 0
 
 func increaseScore(i):
+	print("score")
 	score = score + i
 	
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.text = "Score: " + str(score)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
